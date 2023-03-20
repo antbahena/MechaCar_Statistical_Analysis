c9320800fbf760153eb03b6eeca75f88b722c67b
antbahena <antbahena@gmail.com>
2023-03-19 23:03:41 -0700

    update code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,30 @@ mpg_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clea
 
 # P-value r-squared Summary
 summary(mpg_lm)
+
+#Import Suspension_Coil.csv
+suspension_coil_df <- read.csv("Suspension_Coil.csv", header = TRUE)
+
+# Mean, Median, Var, SD
+total_summary <- suspension_coil_df %>%
+  summarize(Mean = mean(PSI),
+            Median = median(PSI),
+            Variance = var(PSI),
+            SD = sd(PSI))
+
+# Summary PSI
+total_summary
+
+# Lot Summary (Group_by)
+lot_summary <- suspension_coil_df %>%
+  group_by(Manufacturing_Lot) %>%
+  summarize(Mean = mean(PSI),
+            Median = median(PSI),
+            Variance = var(PSI),
+            SD = sd(PSI))
+
+#Show lot_summary
+print(lot_summary)
+
+
+
